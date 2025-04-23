60a307a606fe3604c628ed0bfb8bbc9693d1539f
Alan Basilio <2319754+alanbasilio@users.noreply.github.com>
2025-04-23 01:53:20 -0300

    updating copy

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         </ol>
 
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
-          <Button asChild>
+          <Button asChild size='lg'>
             <a
               href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
               target='_blank'
@@ -50,7 +50,7 @@ export default function Home() {
               Deploy now
             </a>
           </Button>
-          <Button asChild>
+          <Button asChild size='lg'>
             <a
               href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
               target='_blank'
@@ -60,60 +60,58 @@ export default function Home() {
             </a>
           </Button>
         </div>
-
-        <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
-          <Button asChild>
-            <a
-              href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Image
-                aria-hidden
-                src='/file.svg'
-                alt='File icon'
-                width={16}
-                height={16}
-              />
-              {t('learn')}
-            </a>
-          </Button>
-          <Button asChild>
-            <a
-              href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Image
-                aria-hidden
-                src='/window.svg'
-                alt='Window icon'
-                width={16}
-                height={16}
-              />
-              {t('examples')}
-            </a>
-          </Button>
-          <Button asChild>
-            <a
-              href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Image
-                aria-hidden
-                src='/globe.svg'
-                alt='Globe icon'
-                width={16}
-                height={16}
-              />
-              {t('footer-text')}
-            </a>
-          </Button>
-        </footer>
-
         <LanguageSwitcher />
       </main>
+      <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
+        <Button asChild variant='link' className='px-0'>
+          <a
+            href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Image
+              aria-hidden
+              src='/file.svg'
+              alt='File icon'
+              width={16}
+              height={16}
+            />
+            {t('learn')}
+          </a>
+        </Button>
+        <Button asChild variant='link' className='px-0'>
+          <a
+            href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Image
+              aria-hidden
+              src='/window.svg'
+              alt='Window icon'
+              width={16}
+              height={16}
+            />
+            {t('examples')}
+          </a>
+        </Button>
+        <Button asChild variant='link' className='px-0'>
+          <a
+            href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Image
+              aria-hidden
+              src='/globe.svg'
+              alt='Globe icon'
+              width={16}
+              height={16}
+            />
+            {t('footer-text')}
+          </a>
+        </Button>
+      </footer>
     </div>
   );
 }
